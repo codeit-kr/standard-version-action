f7ba9f67e7c31f9ec77859cf21b901033ecfa3f9
taesoon-macbookpro <jts0462@gmail.com>
2023-12-27 17:03:15 +0900

    feat: add mongo lock

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const core = require('@actions/core');
 const standardVersion = require('standard-version');
+const mongoose = require('mongoose');
 const { getConfiguration } = require('standard-version/lib/configuration');
 
 
 async function run() {
   try {
+    mongoose.connect("mongodb+srv://standard-version-sync:XzAiK9zJkPc8KCY2@standard-version-sync.glsnip8.mongodb.net/standard-version-sync")
+    const Schema = mongoose.Schema;
+    const Sync = new Schema({
+      count: Number,
+    });
+    const SyncModel = mongoose.model('sync', Sync);
+    let found = undefined
+
+    const intervalId = setInterval(async () => {
+      found = await SyncModel.findOne({})
+      if (found.count == 0) {
+        clearInterval(intervalId)
+      }
+    }, 1000);
+
+    found = await SyncModel.findOneAndUpdate({}, {count: found.count + 1})
     await standardVersion(getConfiguration());
+    await SyncModel.findOneAndUpdate({}, {count: found.count - 1})
+    
   } catch (error) {
     core.setFailed(error.stack);
   }
